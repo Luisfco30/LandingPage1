92b332b43772a43e0cd632ea1dd84959bd807d4c
Luis Ponce <palf30@gmail.om>
2022-03-03 19:54:21 -0600

    DEV

--- Practica2/index.js
+++ Practica2/index.js
@@ -3,8 +3,8 @@ var navegador = document.getElementById('navegador')
 var datos = navegador.getElementsByTagName('li')
 
 function obtenerDatos(){
-datos[0].innerHTML =`El navegador es ${navigator.appCodeName}`
-datos[1].innerHTML =`El navegador es ${navigator.appVersion}`
-datos[2].innerHTML =`El navegador es ${navigator.onLine}`
-datos[3].innerHTML =`El navegador es ${navigator.platform}`
+datos[0].innerHTML = navigator.appCodeName
+datos[1].innerHTML = navigator.appVersion
+datos[2].innerHTML ='Conexión: '+ navigator.onLine
+datos[3].innerHTML = 'Plataforma: ' + navigator.platform
 }
