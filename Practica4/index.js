67da30b3bd71ab1536d6b1a666240640b3201277
Luis Ponce <palf30@gmail.om>
2022-03-10 18:50:57 -0600

    imagemapa

--- Practica4/index.js
+++ Practica4/index.js
@@ -11,5 +11,5 @@ function muestraPosicion(posicion) {
     let coordenadas = {x:posicion.coords.latitude,y:posicion.coords.longitude}
     console.log(coordenadas);
     let imagenMapa = document.getElementById('imagenmapa')
-    imagenMapa = `https://maps.googleapis.com/maps/api/staticmap/?center=${coordenadas.x},${coordenadas.y}&zoom=18&size=800x800&key=AIzaSyCMrQgga-C5zCuZLTVk2MPVzX7naqKZXZU`
+    imagenMapa = `https://maps.googleapis.com/maps/api/staticmap?center=${coordenadas.x},${coordenadas.y}&zoom=18&size=800x800&key=AIzaSyCMrQgga-C5zCuZLTVk2MPVzX7naqKZXZU`
 }
