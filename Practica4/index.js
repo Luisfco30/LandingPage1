9b3caa30c73bb4577fe29ed70e642092a4692082
Luis Ponce <palf30@gmail.om>
2022-03-10 18:48:37 -0600

    imagemapa

---
+++ Practica4/index.js
@@ -0,0 +1,15 @@
+var mapa = document.getElementById('mapa')
+
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(muestraPosicion);
+    }
+}
+
+function muestraPosicion(posicion) {
+    let coordenadas = {x:posicion.coords.latitude,y:posicion.coords.longitude}
+    console.log(coordenadas);
+    let imagenMapa = document.getElementById('imagenmapa')
+    imagenMapa = `https://maps.googleapis.com/maps/api/staticmap/?center=${coordenadas.x},${coordenadas.y}&zoom=18&size=800x800&key=AIzaSyCMrQgga-C5zCuZLTVk2MPVzX7naqKZXZU`
+}
