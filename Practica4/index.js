9eb7c4ab8716742c36312743b2850fbdc0adba07
Luis Francisco <66529199+Luisfco30@users.noreply.github.com>
2022-03-11 07:40:28 -0600

    imagenmapa

--- Practica4/index.js
+++ Practica4/index.js
@@ -11,5 +11,5 @@ function muestraPosicion(posicion) {
     let coordenadas = posicion.coords.latitude + ',' + posicion.coords.longitude
     console.log(coordenadas);
     let imagenMapa = document.getElementById('imagenmapa')
-    imagenMapa = `https://maps.googleapis.com/maps/api/staticmap?center=${coordenadas}&zoom=18&size=800x800&key=AIzaSyCMrQgga-C5zCuZLTVk2MPVzX7naqKZXZU`
+    imagenMapa.src = `https://maps.googleapis.com/maps/api/staticmap?center=${coordenadas}&zoom=18&size=800x800&key=AIzaSyCMrQgga-C5zCuZLTVk2MPVzX7naqKZXZU`
 }
