0ca1d8a1de4b1d0dbdb66360f601a0a653019e15
Luis Ponce <palf30@gmail.om>
2022-03-17 18:37:58 -0600

    DEV

---
+++ Practica6/index.js
@@ -0,0 +1,20 @@
+var map
+
+var coordenadas = {
+    lat: 0,
+    lng: 0
+}
+var propiedades = {
+    center: coordenadas,
+    zoom: 20
+}
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), propiedades)
+    var iconBase = 'https://maps.google.com/mapfiles/kml/shapes/';
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        icon: iconBase + 'parking_lot_maps.png'
+    });
+}
